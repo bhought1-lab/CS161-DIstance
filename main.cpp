136fde0ba08a0a548c6b78e18ad25f1c1144594d
bhought1-lab <bhought1@my.chemeketa.edu>
2026-01-12 23:53:10 -0800

    Update main.cpp
    little complicated but I think it works better than the basic math

--- main.cpp
+++ main.cpp
@@ -6,8 +6,33 @@ int main()
 {
 int inches;
 cin >> inches;
+//define math so code can use these instead of the input
+int in_per_foot = 12;
+int ft_per_yrd = 3;
+int yrd_per_mi = 1760;
 
-int inperfoot = 12;
+  //conversion for inches to mesurements for yard and mile
+int in_per_yrd = in_per_foot * ft_per_yrd;
+int in_per_mi = in_per_yrd * yrd_per_mi;
 
+  //math for input inches to output in peramiters
+int miles = inches / in_per_mi;
+inches = inches % in_per_mi;
+
+int yards = inches / in_per_yrd;
+inches = inches % in_per_yrd;
+
+int feet = inches / in_per_ft;
+inches = inches % in_per_ft;
+
+// output code
+
+cout << miles << "miles" << endl;
+cout << yards << "yards" << endl;
+cout << feet << "feet" << endl;
+cout << inches << "inches" << endl;
+
+return 0; 
 }
+
 
